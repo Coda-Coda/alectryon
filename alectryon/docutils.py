d5145b2b6b465e3ac447c4113c22c15aa6a91327
Clément Pit-Claudel <clement.pitclaudel@live.com>
2022-06-14 09:19:30 -0700

    docutils: Make :difficulty: option optional

--- alectryon/docutils.py
+++ alectryon/docutils.py
@@ -814,10 +814,8 @@ class ExperimentalExerciseDirective(Topic, AlectryonDirective):
 
     def run(self):
         [node] = super().run()
-        node['difficulty'] = difficulty = self.options.get('difficulty')
+        node['difficulty'] = self.options.get('difficulty')
         node['optional'] = self.options.get('optional', False)
-        if not difficulty:
-            return self._error("Missing required option ':difficulty:'")
         for title in node.traverse(nodes.title):
             title.children.insert(0, nodes.Text("Exercise: "))
         return [node]
